a26a8cc9a40fd109ba8ded06f8eeecf3814e6d23
Michael Galloway <stick.mick@gmail.com>
2023-01-28 22:09:22 +1000

    Add packages and language servers

---
+++ lua/stick/packer.lua
@@ -0,0 +1,23 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  -- or                            , branch = '0.1.x',
+	requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use ({
+	  'joshdick/onedark.vim',
+	  as = 'onedark',
+	  config = function()
+		  vim.cmd('colorscheme onedark')
+	  end 
+  })
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+end)
